178508a9d9dd0370fe2ce7d958694b7be71b0378
Min-Huang <minh.huang96@gmail.com>
2019-10-23 17:22:01 -0700

    changed to client

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
       directory: "./database/migrations"
     }
   },
-  production: {
+  client: {
     client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
